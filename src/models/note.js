08f38dc7e0aae6bbe5e666931247927249a47ef8
Vladyslav Daletskyi <63199353+daletskyiv@users.noreply.github.com>
2026-07-31 19:43:43 +0300

    create branch 02-mongodb, add all structure

---
+++ src/models/note.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+
+const notesSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, trim: true },
+    tag: {
+      type: String,
+      default: 'Todo',
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+    },
+  },
+  { timestamps: true },
+);
+
+export const Note = model('note', notesSchema);
